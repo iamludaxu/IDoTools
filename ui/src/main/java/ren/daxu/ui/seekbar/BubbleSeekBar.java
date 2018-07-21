0777b0a9837eda256326600927be459bb78f98fd
陆大旭 <iamludaxu@163.com>
2018-07-21 21:14:01 +0800

    增加BubbleSeekBar的方法

--- ui/src/main/java/ren/daxu/ui/seekbar/BubbleSeekBar.java
+++ ui/src/main/java/ren/daxu/ui/seekbar/BubbleSeekBar.java
@@ -11,6 +11,7 @@ import android.graphics.Rect;
 import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.os.Environment;
+import android.support.annotation.ColorInt;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.Gravity;
@@ -39,25 +40,25 @@ public class BubbleSeekBar extends View {
     private float mProgress;//当前量
 
     private Drawable mThumb;//轨道
-    private float mThumbHeight;//拇指高度
-    private float mThumbWidth;//拇指宽度
+    private int mThumbHeight;//拇指高度
+    private int mThumbWidth;//拇指宽度
     private int mThumbTextColor;//拇指文字颜色
     private int mThumbTextSize;//拇指文字大小
     private String mThumbText;//拇指上的文字
 
     private Drawable mTrack;//轨道
     private Drawable mSecondTrack;//二层轨道
-    private float mTrackHeight;//轨道高度
-    private float mTrackMarginLeft;//轨道左边空余
-    private float mTrackMarginRight;//轨道右边空余
+    private int mTrackHeight;//轨道高度
+    private int mTrackMarginLeft;//轨道左边空余
+    private int mTrackMarginRight;//轨道右边空余
 
-    private float mThumbOffset;//拇指偏移量
+    private int mThumbOffset;//拇指偏移量
     private float mTrackCenterY;//轨道中心Y坐标
     private boolean isThumbOnDragging = false;
 
     private int mBubbleOffset;
 
-    private float mTrackLength;
+    private int mTrackLength;
     private Paint mPaint;
     private WindowManager mWindowManager;
     private FrameLayout mBubbleFL;
@@ -82,7 +83,7 @@ public class BubbleSeekBar extends View {
         mTrackMarginRight = a.getDimensionPixelSize(R.styleable.DaXuBubbleSeekBar_trackMarginRight, 0);
         mThumbTextColor = a.getColor(R.styleable.DaXuBubbleSeekBar_thumbTextColor, Color.BLACK);
         mThumbTextSize = a.getDimensionPixelSize(R.styleable.DaXuBubbleSeekBar_thumbTextSize, 14);
-        mTrackHeight = a.getDimension(R.styleable.DaXuBubbleSeekBar_barHeight, 10);
+        mTrackHeight = a.getDimensionPixelSize(R.styleable.DaXuBubbleSeekBar_barHeight, 10);
         mTrack = a.getDrawable(R.styleable.DaXuBubbleSeekBar_track);
         mSecondTrack = a.getDrawable(R.styleable.DaXuBubbleSeekBar_secondTrack);
         mBubbleOffset = a.getDimensionPixelOffset(R.styleable.DaXuBubbleSeekBar_bubbleOffset, 10);
@@ -132,10 +133,10 @@ public class BubbleSeekBar extends View {
         int measureWidth = getLayoutParams().width;
         int measureHeight = getLayoutParams().height;
         if (measureHeight == ViewGroup.LayoutParams.WRAP_CONTENT) {
-            measureHeight = (int) (mThumbHeight > mTrackHeight ? mThumbHeight : mTrackHeight);
+            measureHeight = mThumbHeight > mTrackHeight ? mThumbHeight : mTrackHeight;
         }
         if (measureWidth == ViewGroup.LayoutParams.WRAP_CONTENT) {
-            measureWidth = (int) mThumbWidth;
+            measureWidth = mThumbWidth;
         }
         measureWidth = resolveSize(measureWidth, widthMeasureSpec);
         measureHeight = resolveSize(measureHeight, heightMeasureSpec);
@@ -144,55 +145,55 @@ public class BubbleSeekBar extends View {
         mTrackLength = measureWidth - mThumbWidth;
     }
 
-@Override
-public boolean onTouchEvent(MotionEvent event) {
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
 
-    switch (event.getAction()) {
-        case MotionEvent.ACTION_DOWN: {
-            performClick();
-            isThumbOnDragging = true;
-            if (mOnProgressChangedListener != null) {
-                mOnProgressChangedListener.onStartTrackingTouch(this);
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN: {
+                performClick();
+                isThumbOnDragging = true;
+                if (mOnProgressChangedListener != null) {
+                    mOnProgressChangedListener.onStartTrackingTouch(this);
+                }
+                showBubble();
             }
-            showBubble();
-        }
-        break;
-        case MotionEvent.ACTION_MOVE: {
-            mThumbOffset = event.getX() - mThumbWidth / 2;
-            if (mThumbOffset < 0)
-                mThumbOffset = 0;
-            else if (mThumbOffset > mTrackLength)
-                mThumbOffset = mTrackLength;
-            if (mTrackLength != 0)
-                mProgress = mMin + (mMax - mMin) * (mThumbOffset) / mTrackLength;
-            else
-                mProgress = mMin;
-            calculateBubble();
-            postInvalidate();
-            if (mOnProgressChangedListener != null) {
-                mOnProgressChangedListener.onProgressChanged(this, getProgress(), true);
+            break;
+            case MotionEvent.ACTION_MOVE: {
+                mThumbOffset = (int) event.getX() - mThumbWidth / 2;
+                if (mThumbOffset < 0)
+                    mThumbOffset = 0;
+                else if (mThumbOffset > mTrackLength)
+                    mThumbOffset = mTrackLength;
+                if (mTrackLength != 0)
+                    mProgress = mMin + (mMax - mMin) * (mThumbOffset) / mTrackLength;
+                else
+                    mProgress = mMin;
+                calculateBubble();
+                postInvalidate();
+                if (mOnProgressChangedListener != null) {
+                    mOnProgressChangedListener.onProgressChanged(this, getProgress(), true);
+                }
             }
-        }
-        break;
-        case MotionEvent.ACTION_UP:
-        case MotionEvent.ACTION_CANCEL: {
-            if (mOnProgressChangedListener != null) {
-                mOnProgressChangedListener.onStopTrackingTouch(this);
+            break;
+            case MotionEvent.ACTION_UP:
+            case MotionEvent.ACTION_CANCEL: {
+                if (mOnProgressChangedListener != null) {
+                    mOnProgressChangedListener.onStopTrackingTouch(this);
+                }
+                isThumbOnDragging = false;
+                hideBubble();
             }
-            isThumbOnDragging = false;
-            hideBubble();
+            break;
         }
-        break;
+        return isThumbOnDragging | super.onTouchEvent(event);
     }
-    return isThumbOnDragging | super.onTouchEvent(event);
-}
 
     /**
      * 计算Bubble
      */
     private void calculateBubble() {
-        mLayoutParams.x = mPoint[0] + (int) (mThumbOffset) - (int) ((mBubbleFL.getWidth() - mThumbWidth) / 2);
-        mLayoutParams.y = mPoint[1] - mBubbleFL.getHeight() - Math.round(mThumbHeight / 2.0f) - mBubbleOffset;
+        mLayoutParams.x = mPoint[0] + mThumbOffset - (mBubbleFL.getWidth() - mThumbWidth) / 2;
+        mLayoutParams.y = mPoint[1] - mBubbleFL.getHeight() - (mThumbHeight >> 1) - mBubbleOffset;
         mWindowManager.updateViewLayout(mBubbleFL, mLayoutParams);
     }
 
@@ -308,20 +309,153 @@ public boolean onTouchEvent(MotionEvent event) {
     }
 
     /**
+     * Thumb显示的文字
+     *
+     * @param thumbText
+     */
+    public void updateThumbText(String thumbText) {
+        mThumbText = thumbText;
+        invalidate();
+    }
+
+    /**
+     *
+     * 设置Thumb的高度
+     *
+     * @param height
+     */
+    public void setThumbHeight(int height) {
+        mThumbHeight = height;
+        invalidate();
+    }
+
+    /**
+     *
+     * 设置Thumb的宽度
+     *
+     * @param width
+     */
+    public void setThumbWidth(int width) {
+        mThumbWidth = width;
+        invalidate();
+    }
+
+    /**
+     *
+     * 设置Thumb文字颜色
+     *
+     * @param color
+     */
+    public void setThumbTextColor(@ColorInt int color) {
+        mThumbTextColor = color;
+        invalidate();
+    }
+
+    /**
+     *
+     * 设置Thumb文字大小
+     *
+     * @param textSize
+     */
+    public void setThumbTextSize(int textSize) {
+        mThumbTextSize = textSize;
+    }
+
+    /**
+     *
+     * 设置Thumb
+     *
+     * @param thumb
+     */
+    public void setThumb(Drawable thumb) {
+        mThumb = thumb;
+        invalidate();
+    }
+
+    /**
+     *
+     * 设置Track
+     *
+     * @param track
+     */
+    public void setTrack(Drawable track) {
+        mTrack = track;
+        invalidate();
+    }
+
+    /**
+     *
+     * 设置SecondTrack
+     *
+     * @param secondTrack
+     */
+    public void setSecondTrack(Drawable secondTrack) {
+        mSecondTrack = secondTrack;
+        invalidate();
+    }
+
+    /**
+     *
+     * 设置track的高度
+     *
+     * @param barHeight
+     */
+    public void setBarHeight(int barHeight) {
+        mTrackHeight = barHeight;
+        invalidate();
+    }
+
+    /**
+     *
+     * 设置Track左右的空白
+     *
+     * @param leftMargin
+     * @param rightMargin
+     */
+    public void setTrackMargin(int leftMargin, int rightMargin) {
+        mTrackMarginLeft = leftMargin;
+        mTrackMarginRight = rightMargin;
+        invalidate();
+    }
+
+
+    /**
+     *
+     * Bubble的宽度
+     *
+     * @param width
+     */
+    public void setBubbleWidth(int width) {
+        mLayoutParams.width = width;
+    }
+
+    /**
+     *
+     * Bubble的高度
+     *
+     * @param height
+     */
+    public void setBubbleHeight(int height) {
+        mLayoutParams.height = height;
+    }
+
+    /**
+     *
+     * Bubble向上的偏移量
+     *
+     * @param bubbleOffset
+     */
+    public void setBubbleOffset(int bubbleOffset) {
+        mBubbleOffset = bubbleOffset;
+    }
+
+    /**
      * 获取当前位置
      *
      * @return
      */
     private float getProgress() {
         return mProgress;
-    }
-
-    /**
-     * @param thumbText
-     */
-    public void updateThumbText(String thumbText) {
-        mThumbText = thumbText;
-        postInvalidate();
     }
 
     /**
@@ -340,7 +474,7 @@ public boolean onTouchEvent(MotionEvent event) {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        mThumbOffset = mTrackLength * (mProgress - mMin) / (mMax - mMin);
+        mThumbOffset = (int) (mTrackLength * (mProgress - mMin) / (mMax - mMin));
         drawTrack(canvas);
         drawThumb(canvas);
     }
@@ -380,11 +514,11 @@ public boolean onTouchEvent(MotionEvent event) {
         int save = canvas.save();
         canvas.translate(0, mTrackCenterY - mTrackHeight / 2.0f);
         if (mTrack != null) {
-            mTrack.setBounds(Math.round(mTrackMarginLeft), 0, getMeasuredWidth() - Math.round(mTrackMarginLeft) - Math.round(mTrackMarginRight), Math.round(mTrackHeight));
+            mTrack.setBounds(mTrackMarginLeft, 0, getMeasuredWidth() - mTrackMarginLeft - mTrackMarginRight, mTrackHeight);
             mTrack.draw(canvas);
         }
         if (mSecondTrack != null) {
-            mSecondTrack.setBounds(Math.round(mTrackMarginLeft), 0, Math.round(mThumbOffset + mThumbWidth / 2.0f) - Math.round(mTrackMarginLeft) - Math.round(mTrackMarginRight), Math.round(mTrackHeight));
+            mSecondTrack.setBounds(mTrackMarginLeft, 0, mThumbOffset + mThumbWidth / 2 - mTrackMarginLeft - mTrackMarginRight, mTrackHeight);
             mSecondTrack.draw(canvas);
         }
         canvas.restoreToCount(save);
